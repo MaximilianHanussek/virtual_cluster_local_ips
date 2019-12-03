c5e55f1b1164cf5bc7b8f955131939494da05975
maximilian_hanussek <maximilian.hanussek@uni-tuebingen.de>
2019-12-03 13:31:27 +0100

    Added and tested zabbix removal of removed node

--- delete_node_from_zabbix.py
+++ delete_node_from_zabbix.py
@@ -2,7 +2,7 @@ import os
 from pyzabbix.api import ZabbixAPI
 
 # Create ZabbixAPI class instance
-pasted_URL = 'http://' + str(os.environ["MASTER_IP_PUBLIC"]) + '/zabbix/'
+pasted_URL = 'http://' + str(os.environ["MASTER_NODE_IP_PUBLIC"]) + '/zabbix/'
 zapi = ZabbixAPI(url=str(pasted_URL), user='admin', password='zabbix')
 
 node_name_removed = str(os.environ["NODE_NAME_REMOVED"])
@@ -19,4 +19,5 @@ parsed_removed_host_id = result_removed_host_id['result'][0]['hostid']
 # Delete node based on its id
 result_delete_node = zapi.do_request('host.delete', [parsed_removed_host_id])
 
-
+# Logout from Zabbix
+zapi.user.logout()
